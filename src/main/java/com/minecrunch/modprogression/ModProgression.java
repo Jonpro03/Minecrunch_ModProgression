0d132cd3c798560fc3efeac4aa32c1e4f441a670
Jon Proietti <jonpro03@gmail.com>
2016-06-01 19:05:00 -0500

    Removed boilerplate. Setup base class

---
+++ src/main/java/com/minecrunch/modprogression/ModProgression.java
@@ -0,0 +1,20 @@
+package com.minecrunch.modprogression;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = ModProgression.MODID, version = ModProgression.VERSION)
+public class ModProgression
+{
+    public static final String MODID = "modprogression";
+    public static final String VERSION = "0.1";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		// some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    }
+}
